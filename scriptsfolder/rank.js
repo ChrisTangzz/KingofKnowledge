c66786739f1cc324c8a35b1d85e1431163e6a4f8
Chris Tang <tangchris24@gmail.com>
2022-01-17 16:43:32 +0800

    js

---
+++ scriptsfolder/rank.js
@@ -0,0 +1,23 @@
+firebase.initializeApp({
+    projectId: "finalhtml"
+  });
+  var db = firebase.firestore();
+  var ref = db.collection("rank");
+  let audioback = new Audio("audio/rank.mp3")
+  let text, count = 1;
+  ref.orderBy('grade', 'desc').get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      text = $("<tr></tr>");
+      text.html("<th scope='row'>" + count + "</th>" + "<td>" + doc.id + "</td>" + "<td>" + doc.data().grade + "</td>" + "</tr>");
+      $("#person").append(text);
+      count++;
+      console.log(doc.id, doc.data());
+    });
+  });
+  function GoBack(){
+    $.cookie("restart",1);
+    window.location.href = "index.html";
+  }
+  function Back(){
+    audioback.play();
+  }
